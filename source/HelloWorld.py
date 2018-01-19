a9a3ad24427276928708371a4599a4f06c261f14
Vrishali <vrish1506@gmail.com>
2018-01-19 11:38:28 +0530

    HelloWorld.py

--- source/HelloWorld.py
+++ source/HelloWorld.py
@@ -2,7 +2,7 @@
 """This module will establish a SSH connection to target box and fetch the file system info """
 
 
-print("Hello World!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+print("Hello World!")
 print("Again Hello World")
 print("Again Again Hello World")
 

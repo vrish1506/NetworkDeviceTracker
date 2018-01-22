25f7886f5abaa4fcdc012d17904512c720c5c622
Vrishali <vrish1506@gmail.com>
2018-01-22 14:04:49 +0530

    Made a change

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ PRODUCT_FULL_NAME=${PRODUCT_NAME}${DATE}".tar.gz"
 tar -zcvf ${PRODUCT_FULL_NAME} source/product.py build.sh execute.sh
 echo ${PRODUCT_FULL_NAME}
 
-echo "Extracting..."
+echo "Extracting...${PRODUCT_FULL_NAME}"
 
 tar -zxvf ${PRODUCT_FULL_NAME}

6dbd271db682018916633e67528c1895b4ec2aac
Vrishali <vrish1506@gmail.com>
2018-01-22 17:24:39 +0530

    source/product.py

--- source/product.py
+++ source/product.py
@@ -5,6 +5,7 @@ for i in range(1, 10):
     print ("I am runing, this is just a demostration.")
 
 
+##This is add function
 def Add(number1, number2):
     number3 = number1 + number2
     print("Added Numbers = %d" % number3)

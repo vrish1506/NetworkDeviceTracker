1f70da12bc20ddee342ee993c1158ad1822cf658
Vrishali <vrish1506@gmail.com>
2018-01-22 15:49:51 +0530

    build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,27 @@
 #! /bin/bash
 DATE=`date | sed 's/ /_/g' | sed 's/:/_/g'`
 PRODUCT_NAME="PRODUCT_"
-PRODUCT_FULL_NAME=${PRODUCT_NAME}${DATE}".tar.gz"
+PRODUCT_FULL_NAME=${PRODUCT_NAME}${DATE}.tar
 
-tar -zcvf ${PRODUCT_FULL_NAME} source/product.py build.sh execute.sh
 echo ${PRODUCT_FULL_NAME}
 
-echo "Extracting...${PRODUCT_FULL_NAME}"
+tar -cvf ${PRODUCT_FULL_NAME} ./source/product.py ./build.sh ./execute.sh
 
-tar -zxvf ${PRODUCT_FULL_NAME}
+if [ "$?" = "0" ]; then
+    echo ".tar is created successfully"
+else
+    echo "error in creating .tar"
+    exit 1
+fi
+
+tar -xf ${PRODUCT_FULL_NAME} -C /home/ubuntu/JenkinsQA-NetworkDeviceTracker
+
+
+#tar -cf ${PRODUCT_FULL_NAME}/a.tar /home/ubuntu/qq/NetworkDeviceTracker/source/product.py /home/ubuntu/qq/NetworkDeviceTracker/build.sh /home/ubuntu/qq/NetworkDeviceTracker/execute.sh
+#echo ${PRODUCT_FULL_NAME}
+
+#echo "Extracting...${PRODUCT_FULL_NAME}"
+
+#tar -zxvf ${PRODUCT_FULL_NAME}
 
 

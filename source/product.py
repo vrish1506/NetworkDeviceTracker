8d813a1515a8ceda522e2afe98556464608ea28e
Vrishali <vrish1506@gmail.com>
2018-01-22 14:19:06 +0530

    Made a change

--- source/product.py
+++ source/product.py
@@ -8,7 +8,11 @@ def Add(number1, number2):
     number3 = number1 + number2
     print("Added Numbers = %d" % number3)
 
+def subtract(number1, number2):
+    number3 = number1 - number2
+    print("Subtracted Numbers = %d" % number3)
 
 
 
 
+subtract(5, 1)
